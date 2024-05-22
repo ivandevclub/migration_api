2f1872e0b2045232d1b92ad1bbb8e301f326dd97
ivandev <imacedonio@sportclub.team>
2024-05-22 09:57:01 -0300

    feature/ add pagination

--- migrations_mainapp/views.py
+++ migrations_mainapp/views.py
@@ -3,19 +3,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination
 from .serializers import SocioSerializer
 from .models import Socio
 
 class GetCollectionView(APIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = SocioSerializer
+
     
     def get(self, request):
         try:
             socios_instance = Socio.objects.all()
-            if socios_instance is not None:
-                serialized_data = self.serializer_class(instance=socios_instance, many=True)
-                return Response({"data": serialized_data.data}, status=status.HTTP_200_OK)
+            paginator = LimitOffsetPagination()
+            paginated_data = paginator.paginate_queryset(socios_instance, request, view=self)
+            
+            if paginated_data is not None:
+                serialized_data = self.serializer_class(instance=paginated_data, many=True)
+                return paginator.get_paginated_response(serialized_data.data)
             else:
                 return Response("No data found", status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
