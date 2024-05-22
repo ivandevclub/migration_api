2db131d9f4ca3b6214890797f598417a564cd420
ivandev <imacedonio@sportclub.team>
2024-05-22 09:23:25 -0300

    feature/connect to mongo

--- migration_api/settings.py
+++ migration_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'migration_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'sportclub_dev',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': os.getenv('MONGO_URI')
+        }
     }
 }
 
--- migrations_mainapp/views.py
+++ migrations_mainapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import status
+from .serializers import SocioSerializer
+from .models import Socio
 
-# Create your views here.
+class GetCollectionView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SocioSerializer
+    
+    def get(self, request):
+        try:
+            socios_instance = Socio.objects.all()
+            if socios_instance is not None:
+                serialized_data = self.serializer_class(instance=socios_instance, many=True)
+                return Response({"data": serialized_data.data}, status=status.HTTP_200_OK)
+            else:
+                return Response("No data found", status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    
